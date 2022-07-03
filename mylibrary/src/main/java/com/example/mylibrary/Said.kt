78a06dd3ea99b392a3d123fa60010a71ddfb427f
kama_001 <krahmatullayev69@gmail.com>
2022-07-02 12:46:33 -1200

    Initial commit

---
+++ mylibrary/src/main/java/com/example/mylibrary/Said.kt
@@ -0,0 +1,20 @@
+package com.example.mylibrary
+
+object Said {
+
+    fun plus(a: Int, b: Int): Int {
+        return a + b
+    }
+}
+
+        fun minus(a: Int, b: Int): Int {
+            return a - b
+        }
+
+        fun divide(a: Int, b: Int): Int {
+            return a / b
+        }
+
+        fun multiply(a: Int, b: Int): Int {
+            return a * b
+        }
